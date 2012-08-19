a144fc500d2c428f4d1375516c35d0825a35fae0
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2012-08-20 00:08:12 +0200

    sync_test: Use -fno-strict-aliasing
    Use -fno-strict-aliasing here so we don't have to use it globally.
    Performance on tests isn't important anyway.
    
    Change-Id: I92ae7209ced60176cc5608f32b8ff6a911c09f10
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- libsync/Android.mk
+++ libsync/Android.mk
@@ -12,4 +12,5 @@ LOCAL_SRC_FILES := sync.c sync_test.c
 LOCAL_MODULE := sync_test
 LOCAL_MODULE_TAGS := optional tests
 LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_CFLAGS += -fno-strict-aliasing
 include $(BUILD_EXECUTABLE)
